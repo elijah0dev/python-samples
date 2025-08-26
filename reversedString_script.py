1582d3e9455e0e2b817e2cfd3050c616824e3bf8
Elijah Nieves <124934959+elijah0dev@users.noreply.github.com>
2025-08-26 03:39:34 +0000

    Update reversedString_script.py

--- reversedString_script.py
+++ reversedString_script.py
@@ -1,7 +1,9 @@
+# Python script to reverse a string entered by the user
 string = input("Enter a string: ")
 reversed = ""
 
 for character in string:
     reversed = character + reversed
+
 
 print(reversed)
