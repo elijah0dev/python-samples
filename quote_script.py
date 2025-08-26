4fc513f30c87dc7e3945a52ad00d02bb296f1065
Elijah Nieves <124934959+elijah0dev@users.noreply.github.com>
2025-08-26 03:38:14 +0000

    Update quote_script.py

--- quote_script.py
+++ quote_script.py
@@ -1,6 +1,10 @@
-quotes = ["To be, or not to be, that is the question. William Shakespeare",
-          "That’s one small step for a man, one giant leap for mankind. Neil Armstrong",
-          "I think, therefore I am. René Descartes"]
+# Python script demonstrating lists and loops
+
+quotes = [
+    "To be, or not to be, that is the question. William Shakespeare",
+    "That’s one small step for a man, one giant leap for mankind. Neil Armstrong",
+    "I think, therefore I am. René Descartes"
+]
 
 for quote in quotes:
-    print(quote)
+    print('"' + quote + '"')
